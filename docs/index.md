2a290aadca58116870f71fc3954c435540765c6b
luca.ferrera <luca.ferrera@mia-platform.eu>
2019-12-09 14:49:36 +0100

    create Node.js PrePost Microservice Example

---
+++ docs/index.md
@@ -0,0 +1,4 @@
+# %CUSTOM_PLUGIN_SERVICE_NAME%
+
+A microservice for sending a Slack notification via a
+post decorator.
---
+++ tests/index.test.js
@@ -0,0 +1,149 @@
+/*
+ * Copyright 2019 Mia srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict'
+
+const t = require('tap')
+const nock = require('nock')
+const lc39 = require('@mia-platform/lc39')
+
+async function setupFastify(envVariables) {
+  const fastify = await lc39('./index.js', {
+    // silent => trace for enabling logs
+    logLevel: 'silent',
+    envVariables,
+  })
+  return fastify
+}
+
+t.test('testnode', async t => {
+  nock.disableNetConnect()
+  const fastify = await setupFastify({
+    USERID_HEADER_KEY: 'userid',
+    GROUPS_HEADER_KEY: 'groups',
+    CLIENTTYPE_HEADER_KEY: 'clienttype',
+    BACKOFFICE_HEADER_KEY: 'backoffice',
+    MICROSERVICE_GATEWAY_SERVICE_NAME: 'microservice-gateway.example.org',
+  })
+
+  t.tearDown(async() => {
+    await fastify.close()
+    nock.enableNetConnect()
+  })
+
+  const who = 'John Appleseed'
+  const msg = 'hello to everyone!'
+
+  t.test('POST /notify', t => {
+    t.test('Send a message to the service for sending Slack notification', async t => {
+      const scope = nock('http://api-gateway:8080').
+        post('/notify-slack', { text: `${who} says: ${msg}` })
+        .reply(200, {})
+      const response = await fastify.inject({
+        method: 'POST',
+        url: '/notify',
+        body: {
+          request: {
+            method: '',
+            path: '',
+            headers: {},
+            body: {
+              who,
+              msg,
+            },
+            query: {},
+          },
+          response: {
+            statusCode: 200,
+            headers: {},
+            body: {},
+            query: {},
+          },
+        },
+      })
+      t.equal(response.statusCode, 204)
+      scope.done()
+      t.end()
+    })
+
+    t.test('Do not block the call if the Slack service respond with error', async t => {
+      const scope = nock('http://api-gateway:8080').
+        post('/notify-slack', { text: `${who} says: ${msg}` })
+        .replyWithError('Somenthing went wrong!')
+      const response = await fastify.inject({
+        method: 'POST',
+        url: '/notify',
+        body: {
+          request: {
+            method: '',
+            path: '',
+            headers: {},
+            body: {
+              who,
+              msg,
+            },
+            query: {},
+          },
+          response: {
+            statusCode: 200,
+            headers: {},
+            body: {},
+            query: {},
+          },
+        },
+      })
+
+      t.equal(response.statusCode, 204)
+      scope.done()
+      t.end()
+    })
+
+    t.end()
+  })
+
+  t.test('POST /checkwho', t => {
+    t.test('If "who" is undefined add the default value', async t => {
+      const defaultWho = 'John Doe'
+      const response = await fastify.inject({
+        method: 'POST',
+        url: '/checkwho',
+        body: {
+          method: '',
+          path: '',
+          headers: {},
+          query: {},
+          body: {
+            who: undefined,
+            msg,
+          },
+        },
+      })
+      const expectedResponseBody = {
+        body: {
+          msg,
+          who: defaultWho,
+        },
+      }
+      t.equal(response.statusCode, 200)
+      t.same(JSON.parse(response.body), expectedResponseBody)
+      t.end()
+    })
+
+    t.end()
+  })
+
+  t.end()
+})
